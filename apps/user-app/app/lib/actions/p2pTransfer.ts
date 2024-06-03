dba675f162c0dbcc3d266115024e6e184c29f34e
Ambesh <ambeshgaunker@gmail.com>
2024-06-03 13:02:08 +0530

    testing github workflow

--- apps/user-app/app/lib/actions/p2pTransfer.ts
+++ apps/user-app/app/lib/actions/p2pTransfer.ts
@@ -4,6 +4,7 @@ import { authOptions } from "../auth";
 import prisma from "@repo/db/client";
 
 export async function p2pTransfer(to: string, amount: number) {
+  console.log("GITHUB WORKFLOW example change");
   const session = await getServerSession(authOptions);
   const from = session?.user?.id;
   if (!from) {
